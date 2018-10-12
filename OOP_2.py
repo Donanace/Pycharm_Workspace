bb11b3a240725d84941fd5911326fa90fe228fa5
Donan <43751353+Donanace@users.noreply.github.com>
2018-10-12 23:06:36 +0800

    new example of class

---
+++ OOP_2.py
@@ -0,0 +1,15 @@
+class Tiger():
+    state = 'the king of the forest'
+    strength = 'so strong'
+    __speed = 'fast'
+
+    def __init__(self,name,age,kind):
+        self.name = name
+        self.age =age
+        self.kind = kind
+
+    def information(self):
+        print('This tiger is named {} , it is {} years old , and it belongs to {}.'.format(self.name,self.age,self.kind))
+
+Nor_tiger = Tiger('kika',9,'northest tiger')
+Nor_tiger.information()
